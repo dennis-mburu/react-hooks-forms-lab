d63fe301be89d34b071306703bbf00d5969f7178
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-07-20 00:51:36 +0300

    Assignment Complete

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,14 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function updateItems(item){
+    setItems([...itemData, item])
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={updateItems} />
     </div>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,8 +3,16 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = useState('');
+  const [goods, setGoods] = useState(items)
+
+  // function updateItems(item){
+  //   setGoods([...goods, item])
+  // }
+
+  console.log(goods)
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -14,15 +22,22 @@ function ShoppingList({ items }) {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
-  });
+  }).filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()));
+
+
+  function handleNameSearch(event){
+    setSearchValue(event.target.value)
+    console.log(searchValue)
+  }
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleNameSearch}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+          <Item key={item.id} name={item.name} category={item.category} search={searchValue} />
         ))}
       </ul>
     </div>
